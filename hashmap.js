420632072f01c66b54bdb5a33c1e9b423dbc1206
Dylan Simon <grvdylans@gmail.com>
2025-06-01 23:03:01 -0600

    Initial Hash function

---
+++ hashmap.js
@@ -0,0 +1,17 @@
+class HashMap {
+    constructor() {
+        loadFactor;
+        capacity = 16;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    }
+}
