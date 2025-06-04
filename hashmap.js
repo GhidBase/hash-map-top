65f432f39c8349e507f3baf5fd78dbbf85e2e114
Dylan Simon <grvdylans@gmail.com>
2025-06-04 00:42:42 -0600

    Resize works

--- hashmap.js
+++ hashmap.js
@@ -148,19 +148,37 @@ class HashMap {
 
     resize() {
         /*
-            GOAL: Double the size of this hashmap
-
-            NOTES:
-            Test the load factor
-            Check that against the threshold
-            Adjust the this.capacity field
-            
+            Check if the threshold has been passed by the
+            load factor yet. If it has, then double the capacity
+            of the hashmap and refill it with the new hash.
         */
 
         const currentLength = this.length();
         this.loadFactor = currentLength / this.capacity;
-        console.log(this.loadFactor);
-        return this.loadFactor > this.threshold;
+
+        console.log(`used capacity: ${this.length()}`);
+        console.log(`hashmap capacity: ${this.capacity}`);
+        console.log(`load factor: ${this.checkLoadFactor()}\n`);
+
+        if (this.loadFactor < this.threshold) return false;
+
+        this.capacity *= 2;
+        let newArray = [];
+        let entries = this.entries();
+        this.hashMap = [];
+        // console.log(entries.length);
+        for (let i = 0; i < entries.length; i++) {
+            this.set(entries[i][0], entries[i][1]);
+        }
+
+        console.log(`used capacity: ${this.length()}`);
+        console.log(`hashmap capacity: ${this.capacity}`);
+        console.log(`load factor: ${this.checkLoadFactor()}\n`);
+        return true;
+    }
+
+    checkLoadFactor() {
+        return this.length() / this.capacity;
     }
 
     outOfBoundsCheck(index) {
@@ -190,8 +208,131 @@ hashMap.set("twelfth message", "Benjamin Kim");
 hashMap.set("thirteenth message", "Ava Patel");
 hashMap.set("fourteenth message", "Lucas Nguyen");
 hashMap.set("fifteenth message", "Isabella Wright");
+hashMap.set("sixteenth message", "Ethan Baker");
+hashMap.set("seventeenth message", "Mia Turner");
+hashMap.set("eighteenth message", "William Scott");
+hashMap.set("nineteenth message", "Charlotte Rivera");
+hashMap.set("twentieth message", "James Reed");
+hashMap.set("twenty-first message", "Amelia Perez");
+hashMap.set("twenty-second message", "Henry Cox");
+hashMap.set("twenty-third message", "Harper Ward");
+hashMap.set("twenty-fourth message", "Alexander Brooks");
+hashMap.set("twenty-fifth message", "Evelyn Sanders");
+hashMap.set("twenty-sixth message", "Michael Price");
+hashMap.set("twenty-seventh message", "Abigail Bennett");
+hashMap.set("twenty-eighth message", "Daniel Ross");
+hashMap.set("twenty-ninth message", "Ella Bailey");
+hashMap.set("thirtieth message", "Matthew Rivera");
 
 //#endregion
 
-console.log(hashMap.entries());
+// console.log(hashMap.entries());
+console.log(hashMap.resize());
+
+// console.log(hashMap.hashMap[2].toString());
+
+//#region 100 records
+hashMap.set("message 31", "Aria Morgan");
+hashMap.set("message 32", "Logan Jenkins");
+hashMap.set("message 33", "Chloe Barnes");
+hashMap.set("message 34", "Aiden Perry");
+hashMap.set("message 35", "Zoe Powell");
+hashMap.set("message 36", "Jack Long");
+hashMap.set("message 37", "Lily Patterson");
+hashMap.set("message 38", "Owen Hughes");
+hashMap.set("message 39", "Grace Flores");
+hashMap.set("message 40", "Wyatt Ward");
+hashMap.set("message 41", "Hannah Bryant");
+hashMap.set("message 42", "Sebastian Torres");
+hashMap.set("message 43", "Layla Fisher");
+hashMap.set("message 44", "Gabriel Foster");
+hashMap.set("message 45", "Riley Simmons");
+hashMap.set("message 46", "Julian Butler");
+hashMap.set("message 47", "Nora Gonzalez");
+hashMap.set("message 48", "Levi Hayes");
+hashMap.set("message 49", "Scarlett Bryant");
+hashMap.set("message 50", "David Coleman");
+hashMap.set("message 51", "Aurora Barnes");
+hashMap.set("message 52", "Isaac Bennett");
+hashMap.set("message 53", "Penelope Mitchell");
+hashMap.set("message 54", "Anthony Russell");
+hashMap.set("message 55", "Ellie Griffin");
+hashMap.set("message 56", "Joseph West");
+hashMap.set("message 57", "Hazel Chapman");
+hashMap.set("message 58", "Samuel Henderson");
+hashMap.set("message 59", "Violet Armstrong");
+hashMap.set("message 60", "Caleb Graham");
+hashMap.set("message 61", "Madelyn Wells");
+hashMap.set("message 62", "Nathan Harper");
+hashMap.set("message 63", "Aubrey Stone");
+hashMap.set("message 64", "Christopher Weaver");
+hashMap.set("message 65", "Claire Hudson");
+hashMap.set("message 66", "Eli Gibson");
+hashMap.set("message 67", "Lucy Ford");
+hashMap.set("message 68", "Christian Murray");
+hashMap.set("message 69", "Stella Burns");
+hashMap.set("message 70", "Jonathan Hunter");
+hashMap.set("message 71", "Savannah Carroll");
+hashMap.set("message 72", "Thomas Spencer");
+hashMap.set("message 73", "Brooklyn Ellis");
+hashMap.set("message 74", "Aaron Stone");
+hashMap.set("message 75", "Audrey Lane");
+hashMap.set("message 76", "Charles Warren");
+hashMap.set("message 77", "Camila Banks");
+hashMap.set("message 78", "Jaxon Barker");
+hashMap.set("message 79", "Elena McCoy");
+hashMap.set("message 80", "Hunter Lambert");
+hashMap.set("message 81", "Paisley Walsh");
+hashMap.set("message 82", "Andrew Stephens");
+hashMap.set("message 83", "Anna George");
+hashMap.set("message 84", "Leo Black");
+hashMap.set("message 85", "Sadie Crawford");
+hashMap.set("message 86", "Adrian Morrison");
+hashMap.set("message 87", "Caroline Hansen");
+hashMap.set("message 88", "Josiah Reid");
+hashMap.set("message 89", "Naomi Hicks");
+hashMap.set("message 90", "Connor Henry");
+hashMap.set("message 91", "Samantha Boyd");
+hashMap.set("message 92", "Brayden Arnold");
+hashMap.set("message 93", "Luna Holmes");
+hashMap.set("message 94", "Robert Johnston");
+hashMap.set("message 95", "Ruby Ray");
+hashMap.set("message 96", "Jeremiah Jacobs");
+hashMap.set("message 97", "Isla Franklin");
+hashMap.set("message 98", "Easton Dean");
+hashMap.set("message 99", "Vivienne Riley");
+hashMap.set("message 100", "Jason Barker");
+hashMap.set("message 101", "Mila Walton");
+hashMap.set("message 102", "Diego Newton");
+hashMap.set("message 103", "Cora Gibbs");
+hashMap.set("message 104", "Miles Holt");
+hashMap.set("message 105", "Valentina Arnold");
+hashMap.set("message 106", "Roman Paul");
+hashMap.set("message 107", "Delilah Massey");
+hashMap.set("message 108", "Xavier Holloway");
+hashMap.set("message 109", "Jade Holt");
+hashMap.set("message 110", "Asher Wolfe");
+hashMap.set("message 111", "Ivy Steele");
+hashMap.set("message 112", "Hudson Pope");
+hashMap.set("message 113", "Autumn Norris");
+hashMap.set("message 114", "Dominic Neal");
+hashMap.set("message 115", "Eliza Phelps");
+hashMap.set("message 116", "Brody Peters");
+hashMap.set("message 117", "Faith Erickson");
+hashMap.set("message 118", "Colton Booth");
+hashMap.set("message 119", "Jasmine Horton");
+hashMap.set("message 120", "Silas Matthews");
+hashMap.set("message 121", "Georgia Ryan");
+hashMap.set("message 122", "Bentley Brewer");
+hashMap.set("message 123", "Makayla Drake");
+hashMap.set("message 124", "Gavin Walton");
+hashMap.set("message 125", "Vivian Rice");
+hashMap.set("message 126", "Parker Brady");
+hashMap.set("message 127", "Adeline Terry");
+hashMap.set("message 128", "Bryson Cain");
+hashMap.set("message 129", "Rylee Curtis");
+hashMap.set("message 130", "Everett Lindsey");
+
+//#endregion
+
 console.log(hashMap.resize());
